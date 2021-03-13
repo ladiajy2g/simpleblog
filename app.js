62aa889ea05841164e57b0f0b92ec4b457ff64f1
ladiajy2g <ladi.ajayi@gmail.com>
2021-03-13 21:05:02 +0100

    updated all files

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const _ = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,29 +17,54 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://dipo:OskdlUMo6MxD30GQ@cluster0.ymekl.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://dipo:OskdlUMo6MxD30GQ@cluster0.ymekl.mongodb.net/blogDB", {useNewUrlParser: true});
 
-const postsSchema = {
+const postSchema = {
   title: String,
   content: String
 };
 
-const Post = mongoose.model("Post", postsSchema);
-
-// initial sample post for the mongoose database
-// const post1 = new Post({
-//   title: 'My first Post',
-//   content: 'Lacus vel facilisis tor nectuorci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris'
-// });
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
-  
-  Post.find((err, posts)=>{
+
+  Post.find({}, function(err, posts){
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts
       });
-  })
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
 });
 
 app.get("/about", function(req, res){
@@ -50,34 +75,6 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-  const post = new Post ({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-  post.save((err)=>{
-    if(!err) {
-      res.redirect('/');
-    }
-  });
-});
-
-app.get('/:postId', (req, res) => {
-  const requestedId = req.params.postId;
-  Post.findOne({_id: requestedId}, (err, post) => {
-
-    res.render('post', {
-      title: post.title,
-      content: post.content
-    });
-  });
-
-});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
